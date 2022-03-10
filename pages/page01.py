8db77a8fe1f88802cb62cd6c0d537aaf13c77e3c
YonG <99009871+ThianYong@users.noreply.github.com>
2022-03-10 17:49:21 +0800

    Update page01.py

--- pages/page01.py
+++ pages/page01.py
@@ -23,7 +23,7 @@ def app():
     st.write('## ETH Data')
 
     d_parser = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %I-%p')
-    df = pd.read_csv('../Project/data/ETH_1h.csv', parse_dates=['Date'], date_parser=d_parser, index_col='Date')
+    df = pd.read_csv('../data/ETH_1h.csv', parse_dates=['Date'], date_parser=d_parser, index_col='Date')
 
     st.write('## Highest - ETH')
     highs = df['High'].resample('D').max()
