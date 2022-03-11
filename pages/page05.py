8a63a8b816004b2ec5a1428b7adc85ee5822c46d
thianyong <thianyong@gmail.com>
2022-03-11 22:14:08 +0800

    update.

--- pages/page05.py
+++ pages/page05.py
@@ -22,6 +22,7 @@ def app():
 		"""
 		Modern Singapore was founded in 1819 by Sir Stamford Raffles as a trading post of the British Empire. In 1867, the colonies in Southeast Asia were reorganised and Singapore came under the direct control of Britain as part of the Straits Settlements. During the Second World War, Singapore was occupied by Japan in 1942, and returned to British control as a separate crown colony following Japan's surrender in 1945. Singapore gained self-governance in 1959 and in 1963 became part of the new federation of Malaysia, alongside Malaya, North Borneo, and Sarawak. 
 		Ideological differences led to Singapore being expelled from the federation two years later and it became an independent country.
+		
         """
 	)
 
