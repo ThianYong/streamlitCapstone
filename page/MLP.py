16bb4c47c33090d8d44d4d394a7afd7afbe853fd
thianyong <thianyong@gmail.com>
2022-06-17 13:16:00 +0800

    add the graph selection to MLP page.

--- page/MLP.py
+++ page/MLP.py
@@ -114,7 +114,7 @@ def app():
     st.markdown('---')
 
 
-    # ---- Time Bar Chart ---- #
+    # ---- Ti  me Bar Chart ---- #
     time_chart = (
         df_selection.groupby(by=['J']).mean()[['time']]#.sort_values(by='time')
     )
