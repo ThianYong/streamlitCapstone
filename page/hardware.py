0ecb783fdf3d8b64dcbae4156cc7ea067a31aeae
thianyong <thianyong@gmail.com>
2022-07-27 15:17:11 +0800

    update hardware page

--- page/hardware.py
+++ page/hardware.py
@@ -7,7 +7,7 @@ import plotly.figure_factory as ff
 #@st.cache
 def app():
     """
-    What is this page for?
+    On hardware
     """
     st.title(':computer: Hardware Specification') #set the title
 
@@ -35,21 +35,64 @@ def app():
     def get_data(filename):
         return pd.read_csv(filename)
 
+    # ## ---- user select ---- ##
+    # col1, col2 = st.columns(2)
+    #
+    # with col1:
+    #     model = st.selectbox(
+    #         'Select the Model Type: ',
+    #         ('Ising MLP', 'Ising RBM', 'Ising J1-J2 MLP', 'Ising J1-J2 RBM')
+    #     )
+    #
+    #     if model =='Ising MLP':
+    #         df=get_data('data/MLP_combined.csv')
+    #     elif model == 'Ising RBM':
+    #         df=get_data('data/RBM_combined.csv')
+    #     elif model == 'Ising J1-J2 MLP':
+    #         df=get_data('data/RBM_J1J2_combined.csv')
+    #     else:
+    #         df=get_data('data/RBM_J1J2_combined.csv')
+    #
+    # with col2:
+    #     length = st.multiselect(
+    #         'Select the Length: ',
+    #         ['4', '8', '16', '32'],
+    #         default='4'
+    #     )
+
+    # df_selection = df.query(
+    #     'Length == @length & model == @model'
+    # )
+
     ''' Plot the distribution of time consumed. Read in the data '''
     # cols_filt = ['num', 'epoch', 'time', 'J', 'length']
     # df_4 = get_data('MLP_combined_length_4.csv') #, usecols=cols_filt)
     # df_8 = get_data('MLP_combined_length_8.csv') #, usecols=cols_filt)
 
     df = get_data('data/MLP_combined.csv') ### combined all into 1 df.
-    filt_4 = df['length']==4
-    filt_8 = df['length']==8
-    filt_16 = df['length']==16
-    filt_32 = df['length'] ==32
+    filt_4 = df['Length']==4
+    filt_8 = df['Length']==8
+    filt_16 = df['Length']==16
+    filt_32 = df['Length'] ==32
 
-    hist_data = [df[filt_4]['time']]#, df[filt_8]['time'], df[filt_16]['time'], df[filt_32]['time']]
-    group_labels = ['Length = 4']#, 'Length = 8', 'Length = 16', 'Length = 32']
+    hist_data = [df[filt_4]['Time'], df[filt_8]['Time']]#, df[filt_16]['Time'], df[filt_32]['Time']]
+    group_labels = ['Length = 4', 'Length = 8']#, 'Length = 16', 'Length = 32']
     fig = ff.create_distplot(hist_data, group_labels)
     fig.update_layout(title='Distribution Plot on Time(sec) Taken for Each Iteration for Different Sizes - MLP')
+
+    st.plotly_chart(fig, use_container_width=True)
+
+    ## ==== Plot for RBM ==== ##
+    df = get_data('data/RBM_combined.csv') ### combined all into 1 df.
+    filt_4 = df['Length']==4
+    filt_8 = df['Length']==8
+    filt_16 = df['Length']==16
+    filt_32 = df['Length'] ==32
+
+    hist_data = [df[filt_4]['Time'], df[filt_8]['Time']]#, df[filt_16]['Time'], df[filt_32]['Time']]
+    group_labels = ['Length = 4', 'Length = 8']#, 'Length = 16', 'Length = 32']
+    fig = ff.create_distplot(hist_data, group_labels)
+    fig.update_layout(title='Distribution Plot on Time(sec) Taken for Each Iteration for Different Sizes - RBM')
 
     st.plotly_chart(fig, use_container_width=True)
 
